b0649c9c28584a1f6fd89b57779ea548cbf03371
scoobycoder <scoobycoder@gmail.com>
2015-01-11 15:30:20 -0500

    tests around movement commands and adding starting direction

--- src/main/java/mars/MarsRover.java
+++ src/main/java/mars/MarsRover.java
@@ -5,12 +5,19 @@ public class MarsRover {
 	private int x;
 	private int y;
 
-	public MarsRover(int x_coord, int y_coord) {
+	public MarsRover(int x_coord, int y_coord, Direction direction) {
 		this.x = x_coord;
 		this.y = y_coord;
 	}
 
 	public boolean alive() {
+		return true;
+	}
+
+	public boolean move(String[] commands) {
+		if (commands[0] == null || commands[0].equals(""))
+			return false;
+		
 		return true;
 	}
 	
--- src/test/java/mars/MarsRoverTest.java
+++ src/test/java/mars/MarsRoverTest.java
@@ -8,10 +8,36 @@ public class MarsRoverTest {
 
 	@Test
 	public void roverShouldTakeInitialCoordinates() {
-		
-		MarsRover rover = new MarsRover(0,0);
-		
+		MarsRover rover = new MarsRover(0, 0, Direction.NORTH);
+
 		assertEquals(true, rover.alive());
 	}
-	
+
+	@Test
+	public void roverShouldReceiveNullCommandsAndGoNowhere() {
+
+		MarsRover rover = new MarsRover(0, 0, Direction.NORTH);
+		String[] commands = new String[10];
+
+		assertEquals(false, rover.move(commands));
+	}
+
+	@Test
+	public void roverShouldReceiveEmptyCommandsAndGoNowhere() {
+		MarsRover rover = new MarsRover(0, 0, Direction.NORTH);
+		String[] commands = new String[10];
+		commands[0] = "";
+
+		assertEquals(false, rover.move(commands));
+	}
+
+	@Test
+	public void roverShouldReceiveOneCommandAndReportItMoved() {
+		MarsRover rover = new MarsRover(0, 0, Direction.NORTH);
+		String[] commands = new String[10];
+		commands[0] = "N";
+
+		assertEquals(true, rover.move(commands));
+	}
+
 }
