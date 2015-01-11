25c29976810d96bc820ade42d5e1115ff446cd4d
scoobycoder <scoobycoder@gmail.com>
2015-01-11 15:33:53 -0500

    refactor to remove dupes in test

--- src/test/java/mars/MarsRoverTest.java
+++ src/test/java/mars/MarsRoverTest.java
@@ -2,21 +2,26 @@ package mars;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class MarsRoverTest {
 
+	private MarsRover rover;
+
+	@Before
+	public void setup() {
+		rover = new MarsRover(0, 0, Direction.NORTH);
+	}
+	
 	@Test
 	public void roverShouldTakeInitialCoordinates() {
-		MarsRover rover = new MarsRover(0, 0, Direction.NORTH);
-
 		assertEquals(true, rover.alive());
 	}
 
 	@Test
 	public void roverShouldReceiveNullCommandsAndGoNowhere() {
-
-		MarsRover rover = new MarsRover(0, 0, Direction.NORTH);
 		String[] commands = new String[10];
 
 		assertEquals(false, rover.move(commands));
@@ -24,7 +29,6 @@ public class MarsRoverTest {
 
 	@Test
 	public void roverShouldReceiveEmptyCommandsAndGoNowhere() {
-		MarsRover rover = new MarsRover(0, 0, Direction.NORTH);
 		String[] commands = new String[10];
 		commands[0] = "";
 
@@ -33,11 +37,15 @@ public class MarsRoverTest {
 
 	@Test
 	public void roverShouldReceiveOneCommandAndReportItMoved() {
-		MarsRover rover = new MarsRover(0, 0, Direction.NORTH);
 		String[] commands = new String[10];
 		commands[0] = "N";
 
 		assertEquals(true, rover.move(commands));
+	}
+	
+	@Test @Ignore
+	public void roverShouldTurnLeftAndReportSuccess() {
+		
 	}
 
 }
